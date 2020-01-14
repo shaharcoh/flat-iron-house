6fb23cf3208254c11140a1aee2cde1fcf7453e96
Codrin Chelaru <codrin.chelaru@wordsearch.co.uk>
2020-01-14 17:35:39 +0000

    Test edits

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'wordpress');
 define( 'DB_USER', 'root');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password');
+define( 'DB_PASSWORD', 'flat-iron213211');
 
 /** MySQL hostname */
 define( 'DB_HOST', 'mysql');
